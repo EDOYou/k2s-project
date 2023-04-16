1c442e4d70674e9a7511eca150e3e04cfe1e78f7
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-17 01:21:07 +0200

    Repo for the users added

---
+++ src/main/java/com/edoyou/k2sbeauty/entities/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.edoyou.k2sbeauty.entities.repositories;
+
+
+import com.edoyou.k2sbeauty.entities.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  User findByEmail(String email);
+}
