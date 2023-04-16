679aa83e87d4dc68ae112e1f0dd7a2a9dff41a8f
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-16 06:46:35 +0200

    Project set-up Setting-up the project: pom.xml and src folder are added, .gitignore modified so it hides .sh files that help us hiding the credentials

---
+++ src/main/java/com/edoyou/k2sbeauty/K2SbeautyApplication.java
@@ -0,0 +1,12 @@
+package com.edoyou.k2sbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K2SbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K2SbeautyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edoyou/k2sbeauty/K2SbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edoyou.k2sbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K2SbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
