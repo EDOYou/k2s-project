65bdc1f2b0eaca29d50955a5497d02bb7b3a3df9
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-17 01:05:36 +0200

    Role model is implemented

--- src/main/java/com/edoyou/k2sbeauty/model/Role.java
+++ src/main/java/com/edoyou/k2sbeauty/model/Role.java
@@ -1,5 +1,43 @@
 package com.edoyou.k2sbeauty.model;
 
-public class Role {
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "roles")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false, unique = true)
+  private String name;
+
+  @ManyToMany(mappedBy = "roles")
+  private Set<User> users = new HashSet<>();
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setUsers(Set<User> users) {
+    this.users = users;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Set<User> getUsers() {
+    return users;
+  }
 }
