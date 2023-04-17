93a0abd4cf2d4d2a4bd0110b65be537f7d8bbecf
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-17 03:30:50 +0200

    Appointment model added It represents an appointment between a client and a hairdresser. Has many-to-one relationship with the Client, Hairdresser, and Service

---
+++ src/main/java/com/edoyou/k2sbeauty/entities/model/Appointment.java
@@ -0,0 +1,67 @@
+package com.edoyou.k2sbeauty.entities.model;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "appointments")
+public class Appointment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "client_id", nullable = false)
+  private Client client;
+
+  @ManyToOne
+  @JoinColumn(name = "hairdresser_id", nullable = false)
+  private Hairdresser hairdresser;
+
+  @Column(nullable = false)
+  private LocalDateTime appointmentTime;
+
+  @ManyToOne
+  @JoinColumn(name = "service_id", nullable = false)
+  private Service service;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setClient(Client client) {
+    this.client = client;
+  }
+
+  public void setHairdresser(Hairdresser hairdresser) {
+    this.hairdresser = hairdresser;
+  }
+
+  public void setAppointmentTime(LocalDateTime appointmentTime) {
+    this.appointmentTime = appointmentTime;
+  }
+
+  public void setService(Service service) {
+    this.service = service;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public Client getClient() {
+    return client;
+  }
+
+  public Hairdresser getHairdresser() {
+    return hairdresser;
+  }
+
+  public LocalDateTime getAppointmentTime() {
+    return appointmentTime;
+  }
+
+  public Service getService() {
+    return service;
+  }
+}
