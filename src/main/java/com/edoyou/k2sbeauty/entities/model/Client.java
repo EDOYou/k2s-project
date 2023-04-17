bba1375341ee0700c9baeffa6c1f919a7d245697
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-17 03:28:21 +0200

    Clien model added Represents a client in the system

---
+++ src/main/java/com/edoyou/k2sbeauty/entities/model/Client.java
@@ -0,0 +1,20 @@
+package com.edoyou.k2sbeauty.entities.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "clients")
+public class Client extends User {
+  @OneToMany(mappedBy = "client")
+  private Set<Appointment> appointments = new HashSet<>();
+
+  public void setAppointments(Set<Appointment> appointments) {
+    this.appointments = appointments;
+  }
+
+  public Set<Appointment> getAppointments() {
+    return appointments;
+  }
+}
