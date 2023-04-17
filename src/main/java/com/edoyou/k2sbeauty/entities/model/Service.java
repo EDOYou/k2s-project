51acae87f428af00c2fe3ecbcdebaaed94713a4e
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-17 03:10:08 +0200

    Service model added This class represents user role in the system. Roles are going to be used to manage the access control of the application

---
+++ src/main/java/com/edoyou/k2sbeauty/entities/model/Service.java
@@ -0,0 +1,53 @@
+package com.edoyou.k2sbeauty.entities.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "service")
+public class Service {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Column(nullable = false)
+  private double price;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+}
