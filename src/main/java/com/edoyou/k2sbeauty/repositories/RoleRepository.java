604c2229ed7dd943e7a7b48687cb2fc24805e7b9
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-22 14:46:12 +0200

    Role repository added Finding a role by its name.

---
+++ src/main/java/com/edoyou/k2sbeauty/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.edoyou.k2sbeauty.repositories;
+
+import com.edoyou.k2sbeauty.entities.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
