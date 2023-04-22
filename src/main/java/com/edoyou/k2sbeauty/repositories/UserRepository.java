157359e70dbcfddbeecf3852fc93c4ed639635ae
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-04-22 14:08:51 +0200

    User repository added Contains a query method to find users by their e-mail.

---
+++ src/main/java/com/edoyou/k2sbeauty/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.edoyou.k2sbeauty.repositories;
+
+import com.edoyou.k2sbeauty.entities.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
