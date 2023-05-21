07847e0dce549a82c16f8ff66b1ebf61819d1c6a
Taghiyev Kanan <ktaghiyev01@gmail.com>
2023-05-21 11:27:40 +0200

    Feedback service layer is tested.

--- src/main/java/com/edoyou/k2sbeauty/repositories/FeedbackRepository.java
+++ src/main/java/com/edoyou/k2sbeauty/repositories/FeedbackRepository.java
@@ -4,10 +4,5 @@ import com.edoyou.k2sbeauty.entities.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
-  List<Feedback> findByClientId(Long clientId);
-}
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> { }
--- src/test/java/com/edoyou/k2sbeauty/services/implementations/FeedbackServiceImplTest.java
+++ src/test/java/com/edoyou/k2sbeauty/services/implementations/FeedbackServiceImplTest.java
@@ -1,109 +1,95 @@
-//package com.edoyou.k2sbeauty.services;
-//
-//import com.edoyou.k2sbeauty.entities.model.Client;
-//import com.edoyou.k2sbeauty.entities.model.Feedback;
-//import com.edoyou.k2sbeauty.repositories.FeedbackRepository;
-//import com.edoyou.k2sbeauty.services.implementations.FeedbackServiceImpl;
-//import java.util.Collections;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.time.LocalDateTime;
-//import java.util.Arrays;
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class FeedbackServiceImplTest {
-//
-//  @Mock
-//  private FeedbackRepository feedbackRepository;
-//
-//  @InjectMocks
-//  private FeedbackServiceImpl feedbackService;
-//
-//  private Feedback feedback;
-//  private Client client;
-//
-//  @BeforeEach
-//  void setUp() {
-//    client = new Client();
-//    client.setId(1L);
-//
-//    feedback = new Feedback();
-//    feedback.setId(1L);
-//    feedback.setClient(client);
-//    feedback.setComment("Test comment");
-//    feedback.setCreatedAt(LocalDateTime.now());
-//    feedback.setRating(5);
-//  }
-//
-//  @Test
-//  void createFeedbackWithValidData() {
-//    when(feedbackRepository.save(any(Feedback.class))).thenReturn(feedback);
-//
-//    Feedback createdFeedback = feedbackService.createFeedback(feedback);
-//
-//    assertNotNull(createdFeedback);
-//    assertEquals(feedback.getId(), createdFeedback.getId());
-//    assertEquals(feedback.getClient(), createdFeedback.getClient());
-//    assertEquals(feedback.getComment(), createdFeedback.getComment());
-//    assertEquals(feedback.getRating(), createdFeedback.getRating());
-//  }
-//
-//  @Test
-//  void getFeedbackById() {
-//    when(feedbackRepository.findById(feedback.getId())).thenReturn(Optional.of(feedback));
-//
-//    Optional<Feedback> retrievedFeedback = feedbackService.getFeedbackById(feedback.getId());
-//
-//    assertTrue(retrievedFeedback.isPresent());
-//    assertEquals(feedback.getId(), retrievedFeedback.get().getId());
-//  }
-//
-//  @Test
-//  void getAllFeedback() {
-//    when(feedbackRepository.findAll()).thenReturn(Collections.singletonList(feedback));
-//
-//    List<Feedback> feedbackList = feedbackService.getAllFeedback();
-//
-//    assertNotNull(feedbackList);
-//    assertFalse(feedbackList.isEmpty());
-//    assertEquals(1, feedbackList.size());
-//    assertEquals(feedback.getId(), feedbackList.get(0).getId());
-//  }
-//
-//  @Test
-//  void updateFeedback() {
-//    when(feedbackRepository.existsById(feedback.getId())).thenReturn(true);
-//    when(feedbackRepository.save(feedback)).thenReturn(feedback);
-//
-//    Feedback updatedFeedback = feedbackService.updateFeedback(feedback);
-//
-//    assertNotNull(updatedFeedback);
-//    assertEquals(feedback.getId(), updatedFeedback.getId());
-//  }
-//
-//  @Test
-//  void updateNonExistentFeedback() {
-//    when(feedbackRepository.existsById(feedback.getId())).thenReturn(false);
-//
-//    assertThrows(IllegalStateException.class, () -> feedbackService.updateFeedback(feedback));
-//  }
-//
-//  @Test
-//  void getAverageRatingForClient() {
-//    when(feedbackRepository.findByClientId(client.getId())).thenReturn(Arrays.asList(feedback));
-//
-//    double averageRating = feedbackService.getAverageRatingForClient(client.getId());
-//
-//    assertEquals(5, averageRating);
-//  }
-//}
+package com.edoyou.k2sbeauty.services.implementations;
+
+import com.edoyou.k2sbeauty.entities.model.Appointment;
+import com.edoyou.k2sbeauty.entities.model.BeautyService;
+import com.edoyou.k2sbeauty.entities.model.Client;
+import com.edoyou.k2sbeauty.entities.model.Feedback;
+import com.edoyou.k2sbeauty.entities.model.Hairdresser;
+import com.edoyou.k2sbeauty.entities.payment.PaymentStatus;
+import com.edoyou.k2sbeauty.repositories.FeedbackRepository;
+import com.edoyou.k2sbeauty.services.interfaces.HairdresserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class FeedbackServiceImplTest {
+
+  @Mock
+  private FeedbackRepository feedbackRepository;
+
+  @Mock
+  private HairdresserService hairdresserService;
+
+  @InjectMocks
+  private FeedbackServiceImpl feedbackService;
+
+  @Test
+  public void createFeedback_test() {
+    Feedback feedback = createSampleFeedback();
+    Hairdresser hairdresser = feedback.getAppointment().getHairdresser();
+
+    when(feedbackRepository.save(any(Feedback.class))).thenReturn(feedback);
+
+    feedbackService.createFeedback(feedback);
+
+    verify(feedbackRepository, times(1)).save(feedback);
+    verify(hairdresserService, times(1)).updateRating(hairdresser);
+  }
+
+  @Test
+  public void getFeedbackById_test() {
+    Long id = 1L;
+    Feedback feedback = createSampleFeedback();
+
+    when(feedbackRepository.findById(id)).thenReturn(Optional.of(feedback));
+
+    Optional<Feedback> foundFeedback = feedbackService.getFeedbackById(id);
+
+    verify(feedbackRepository, times(1)).findById(id);
+    assertEquals(feedback, foundFeedback.orElse(null));
+  }
+
+  private Feedback createSampleFeedback() {
+    Feedback feedback = new Feedback();
+    feedback.setId(1L);
+    feedback.setComment("Great service!");
+    feedback.setCreatedAt(LocalDateTime.now());
+    feedback.setRating(5);
+
+    Client client = new Client();
+    feedback.setClient(client);
+
+    Appointment appointment = createSampleAppointment();
+    feedback.setAppointment(appointment);
+
+    return feedback;
+  }
+
+  private Appointment createSampleAppointment() {
+    Appointment appointment = new Appointment();
+    appointment.setId(1L);
+    appointment.setAppointmentTime(LocalDateTime.now().plusHours(1));
+    appointment.setCompleted(false);
+    appointment.setPaymentStatus(PaymentStatus.PAID);
+
+    Hairdresser hairdresser = new Hairdresser();
+    hairdresser.setSpecialization("Special");
+    hairdresser.setRating(4.5);
+    appointment.setHairdresser(hairdresser);
+
+    BeautyService beautyService = new BeautyService();
+    appointment.setBeautyService(beautyService);
+
+    return appointment;
+  }
+
+}
